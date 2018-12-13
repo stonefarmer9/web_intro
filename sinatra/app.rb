4f9207e9a5ffdf1e7ccb07cd9ca79a39021a435b
Georges Le Vaillant <georgeslevaillant@Georgess-MacBook-Pro.local>
2018-12-13 09:10:36 +0000

    Testing push on new machine

--- sinatra/app.rb
+++ sinatra/app.rb
@@ -15,10 +15,14 @@ get '/random-cat' do
   erb:index
 end
 
-get '/named-cat' do
+post '/named-cat' do
   p params
   @name = params[:name]
   erb:index
+end
+
+get '/cat-form' do
+  erb :cat_form
 end
 
 get '/bikes' do
